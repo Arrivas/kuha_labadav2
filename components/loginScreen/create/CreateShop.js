86367da498efa27353581fede2ca134c73691aa5
unknown <lolhacker121@gmail.com>
2023-01-07 13:21:09 +0800

    finish first step

--- components/loginScreen/create/CreateShop.js
+++ components/loginScreen/create/CreateShop.js
@@ -3,10 +3,36 @@ import React from "react";
 import FirstStep from "./shop/FirstStep";
 import SafeScreenView from "../../SafeScreenView";
 import CreateShopProgress from "./shop/CreateShopProgress";
+import firebase from "@react-native-firebase/app";
+import "@react-native-firebase/firestore";
+import "@react-native-firebase/auth";
 
 const CreateShop = () => {
-  const handleFirstStepSubmit = () => {
-    console.log("asd");
+  const handleFirstStepSubmit = (val) => {
+    let shopNameErr = "";
+    let shopEmailErr = "";
+    firebase
+      .firestore()
+      .collection("laundryProviders")
+      .where("name", "==", val.name.trim())
+      .limit(1)
+      .get()
+      .then((data) => {
+        if (!data.empty) shopNameErr = "shop name already taken";
+      })
+      .catch((err) => console.log(err, "shop name"));
+    if (shopNameErr) return console.log(shopNameErr);
+    firebase
+      .firestore()
+      .collection("admins")
+      .where("email", "==", val.email.trim())
+      .limit(1)
+      .get()
+      .then((data) => {
+        if (!data.empty) shopEmailErr = "email already taken";
+      })
+      .catch((err) => console.log(err, "shop name"));
+    if (shopEmailErr) return console.log(shopNameErr);
   };
   return (
     <>
